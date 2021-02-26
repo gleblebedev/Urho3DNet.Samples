45b989aa99cf4d396be2d105faa4c60af9015671
RIJX <info@rijx.com>
2021-02-26 10:33:01 +0100

    Add CharacterDemo sample

---
+++ src/Urho3DNet.Samples/CharacterDemo.cs
@@ -0,0 +1,330 @@
+using System;
+
+namespace Urho3DNet.Samples
+{
+    class CharacterDemo : Sample
+    {
+        /// Touch utility object.
+        private Touch touch_;
+        /// The controllable character component.
+        private Character character_;
+        /// First person camera flag.
+        private bool firstPerson_;
+
+        public CharacterDemo(Context context) : base(context)
+        {
+            // Register factory and attributes for the Character component so it can be created via CreateComponent, and loaded / saved
+            Character.RegisterObject(context);
+        }
+
+        public override void Start()
+        {
+            // Execute base class startup
+            base.Start();
+
+            if (touchEnabled_)
+                touch_ = new Touch(Context, TOUCH_SENSITIVITY);
+
+            // Create static scene content
+            CreateScene();
+
+            // Create the controllable character
+            CreateCharacter();
+
+            // Create the UI content
+            CreateInstructions();
+
+            // Subscribe to necessary events
+            SubscribeToEvents();
+
+            // Set the mouse mode to use in the sample
+            InitMouseMode(MouseMode.MmRelative);
+        }
+
+        protected void CreateScene()
+        {
+            Scene = new Scene(Context);
+
+            // Create scene subsystem components
+            Scene.CreateComponent<Octree>();
+            Scene.CreateComponent<PhysicsWorld>();
+
+            // Create camera and define viewport. We will be doing load / save, so it's convenient to create the camera outside the scene,
+            // so that it won't be destroyed and recreated, and we don't have to redefine the viewport on load
+            CameraNode = new Node(Context);
+            var camera = CameraNode.CreateComponent<Camera>();
+            camera.FarClip = 300.0f;
+            Context.Renderer.SetViewport(0, new Viewport(Context, Scene, camera));
+
+            // Create static scene content. First create a zone for ambient lighting and fog control
+            var zoneNode = Scene.CreateChild("Zone");
+            var zone = zoneNode.CreateComponent<Zone>();
+            zone.AmbientColor = new Color(0.15f, 0.15f, 0.15f);
+            zone.FogColor = new Color(0.5f, 0.5f, 0.7f);
+            zone.FogStart = 100.0f;
+            zone.FogEnd = 300.0f;
+            zone.SetBoundingBox(new BoundingBox(-1000.0f, 1000.0f));
+
+            // Create a directional light with cascaded shadow mapping
+            var lightNode = Scene.CreateChild("DirectionalLight");
+            lightNode.Direction = new Vector3(0.3f, -0.5f, 0.425f);
+            var light = lightNode.CreateComponent<Light>();
+            light.LightType = LightType.LightDirectional;
+            light.CastShadows = true;
+            light.ShadowBias = new BiasParameters(0.00025f, 0.5f);
+            light.ShadowCascade = new CascadeParameters(10.0f, 50.0f, 200.0f, 0.0f, 0.8f);
+            light.SpecularIntensity = 0.5f;
+
+            // Create the floor object
+            var floorNode = Scene.CreateChild("Floor");
+            floorNode.Position = new Vector3(0.0f, -0.5f, 0.0f);
+            floorNode.SetScale(new Vector3(200.0f, 1.0f, 200.0f));
+            var floorModel = floorNode.CreateComponent<StaticModel>();
+            floorModel.SetModel(Context.ResourceCache.GetResource<Model>("Models/Box.mdl"));
+            floorModel.SetMaterial(Context.ResourceCache.GetResource<Material>("Materials/Stone.xml"));
+
+            var floorRigidBody = floorNode.CreateComponent<RigidBody>();
+            // Use collision layer bit 2 to mark world scenery. This is what we will raycast against to prevent camera from going
+            // inside geometry
+            floorRigidBody.CollisionLayer = 2;
+            var floorShape = floorNode.CreateComponent<CollisionShape>();
+            floorShape.SetBox(Vector3.One);
+
+            // Create mushrooms of varying sizes
+            const uint NUM_MUSHROOMS = 60;
+            for (uint i = 0; i < NUM_MUSHROOMS; ++i)
+            {
+                var objectNode = Scene.CreateChild("Mushroom");
+                objectNode.Position = new Vector3(MathDefs.Random(180.0f) - 90.0f, 0.0f, MathDefs.Random(180.0f) - 90.0f);
+                objectNode.Rotation = new Quaternion(0.0f, MathDefs.Random(360.0f), 0.0f);
+                objectNode.SetScale(2.0f + MathDefs.Random(5.0f));
+                var objectModel = objectNode.CreateComponent<StaticModel>();
+                objectModel.SetModel(Context.ResourceCache.GetResource<Model>("Models/Mushroom.mdl"));
+                objectModel.SetMaterial(Context.ResourceCache.GetResource<Material>("Materials/Mushroom.xml"));
+                objectModel.CastShadows = true;
+
+                var body = objectNode.CreateComponent<RigidBody>();
+                body.CollisionLayer = 2;
+                var shape = objectNode.CreateComponent<CollisionShape>();
+                shape.SetTriangleMesh(objectModel.GetModel(), 0);
+            }
+
+            // Create movable boxes. Let them fall from the sky at first
+            const uint NUM_BOXES = 100;
+            for (uint i = 0; i < NUM_BOXES; ++i)
+            {
+                float scale = MathDefs.Random(2.0f) + 0.5f;
+
+                var objectNode = Scene.CreateChild("Box");
+                objectNode.Position = new Vector3(MathDefs.Random(180.0f) - 90.0f, MathDefs.Random(10.0f) + 10.0f, MathDefs.Random(180.0f) - 90.0f);
+                objectNode.Rotation = new Quaternion(MathDefs.Random(360.0f), MathDefs.Random(360.0f), MathDefs.Random(360.0f));
+                objectNode.SetScale(scale);
+                var objectModel = objectNode.CreateComponent<StaticModel>();
+                objectModel.SetModel(Context.ResourceCache.GetResource<Model>("Models/Box.mdl"));
+                objectModel.SetMaterial(Context.ResourceCache.GetResource<Material>("Materials/Stone.xml"));
+                objectModel.CastShadows = true;
+
+                var body = objectNode.CreateComponent<RigidBody>();
+                body.CollisionLayer = 2;
+                // Bigger boxes will be heavier and harder to move
+                body.Mass = scale * 2.0f;
+                var shape = objectNode.CreateComponent<CollisionShape>();
+                shape.SetBox(Vector3.One);
+            }
+        }
+
+        protected void CreateCharacter()
+        {
+            var objectNode = Scene.CreateChild("Jack");
+            objectNode.Position = new Vector3(0.0f, 1.0f, 0.0f);
+
+            // spin node
+            var adjustNode = objectNode.CreateChild("AdjNode");
+            adjustNode.Rotation = new Quaternion(180, new Vector3(0, 1, 0));
+
+            // Create the rendering component + animation controller
+            var objectModel = adjustNode.CreateComponent<AnimatedModel>();
+            objectModel.SetModel(Context.ResourceCache.GetResource<Model>("Models/Mutant/Mutant.mdl"));
+            objectModel.SetMaterial(Context.ResourceCache.GetResource<Material>("Models/Mutant/Materials/mutant_M.xml"));
+            objectModel.CastShadows = true;
+            adjustNode.CreateComponent<AnimationController>();
+
+            // Set the head bone for manual control
+            //objectModel.Skeleton.GetBone("Mutant:Head").Animated = false;
+
+            // Create rigidbody, and set non-zero mass so that the body becomes dynamic
+            var body = objectNode.CreateComponent<RigidBody>();
+            body.CollisionLayer = 1;
+            body.Mass = 1.0f;
+
+            // Set zero angular factor so that physics doesn't turn the character on its own.
+            // Instead we will control the character yaw manually
+            body.AngularVelocity = Vector3.Zero;
+
+            // Set the rigidbody to signal collision also when in rest, so that we get ground collisions properly
+            body.CollisionEventMode = CollisionEventMode.CollisionAlways;
+
+            // Set a capsule shape for collision
+            var shape = objectNode.CreateComponent<CollisionShape>();
+            shape.SetCapsule(0.7f, 1.8f, new Vector3(0.0f, 0.9f, 0.0f));
+
+            // Create the character logic component, which takes care of steering the rigidbody
+            // Remember it so that we can set the controls. Use a ea::weak_ptr because the scene hierarchy already owns it
+            // and keeps it alive as long as it's not removed from the hierarchy
+            character_ = objectNode.CreateComponent<Character>();
+        }
+
+        protected void CreateInstructions()
+        {
+            // Construct new Text object, set string to display and font to use
+            var instructionText = Context.UI.Root.CreateChild<Text>();
+            instructionText.SetText(
+                "Use WASD keys and mouse/touch to move\n" +
+                "Space to jump, F to toggle 1st/3rd person\n" +
+                "F5 to save scene, F7 to load"
+            );
+            instructionText.SetFont(Context.ResourceCache.GetResource<Font>("Fonts/Anonymous Pro.ttf"), 15);
+            // The text has multiple rows. Center them in relation to each other
+            instructionText.TextAlignment = HorizontalAlignment.HaCenter;
+
+            // Position the text relative to the screen center
+            instructionText.HorizontalAlignment = HorizontalAlignment.HaCenter;
+            instructionText.VerticalAlignment = VerticalAlignment.VaCenter;
+            instructionText.Position = new IntVector2(0, Context.UI.Root.Height / 4);
+        }
+
+        protected void SubscribeToEvents()
+        {
+            // Subscribe to Update event for setting the character controls before physics simulation
+            SubscribeToEvent(E.Update, HandleUpdate);
+
+            // Subscribe to PostUpdate event for updating the camera position after physics simulation
+            SubscribeToEvent(E.PostUpdate, HandlePostUpdate);
+
+            // Unsubscribe the SceneUpdate event from base class as the camera node is being controlled in HandlePostUpdate() in this sample
+            UnsubscribeFromEvent(E.SceneUpdate);
+        }
+
+        protected void HandleUpdate(StringHash eventType, VariantMap eventData)
+        {
+            if (character_ != null)
+            {
+                // Clear previous controls
+                character_.Controls.Set(Character.CTRL_FORWARD | Character.CTRL_BACK | Character.CTRL_LEFT | Character.CTRL_RIGHT | Character.CTRL_JUMP, false);
+
+                // Update controls using touch utility class
+                if (touch_ != null)
+                    touch_.UpdateTouches(character_.Controls);
+
+                // Update controls using keys
+                if (Context.UI.FocusElement == null)
+                {
+                    if (touch_ == null || !touch_.UseGyroscope)
+                    {
+                        character_.Controls.Set(Character.CTRL_FORWARD, Context.Input.GetKeyDown(Key.KeyW));
+                        character_.Controls.Set(Character.CTRL_BACK, Context.Input.GetKeyDown(Key.KeyS));
+                        character_.Controls.Set(Character.CTRL_LEFT, Context.Input.GetKeyDown(Key.KeyA));
+                        character_.Controls.Set(Character.CTRL_RIGHT, Context.Input.GetKeyDown(Key.KeyD));
+                    }
+                    character_.Controls.Set(Character.CTRL_JUMP, Context.Input.GetKeyDown(Key.KeySpace));
+
+                    // Add character yaw & pitch from the mouse motion or touch input
+                    if (touchEnabled_)
+                    {
+                        for (uint i = 0; i < Context.Input.NumTouches; ++i)
+                        {
+                            var state = Context.Input.GetTouch(i);
+                            if (state.GetTouchedElement() == null)    // Touch on empty space
+                            {
+                                var camera = CameraNode.GetComponent<Camera>();
+                                if (camera == null)
+                                    return;
+
+                                character_.Controls.Yaw += TOUCH_SENSITIVITY * camera.Fov / Context.Graphics.Height * state.Delta.X;
+                                character_.Controls.Pitch += TOUCH_SENSITIVITY * camera.Fov / Context.Graphics.Height * state.Delta.Y;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        character_.Controls.Yaw += (float)Context.Input.MouseMoveX * Character.YAW_SENSITIVITY;
+                        character_.Controls.Pitch += (float)Context.Input.MouseMoveY * Character.YAW_SENSITIVITY;
+                    }
+                    // Limit pitch
+                    character_.Controls.Pitch = MathDefs.Clamp(character_.Controls.Pitch, -80.0f, 80.0f);
+                    // Set rotation already here so that it's updated every rendering frame instead of every physics frame
+                    character_.Node.Rotation = new Quaternion(character_.Controls.Yaw, Vector3.Up);
+
+                    // Switch between 1st and 3rd person
+                    if (Context.Input.GetKeyPress(Key.KeyF))
+                        firstPerson_ = !firstPerson_;
+
+                    // Turn on/off gyroscope on mobile platform
+                    if (touch_ != null && Context.Input.GetKeyPress(Key.KeyG))
+                        touch_.UseGyroscope = !touch_.UseGyroscope;
+
+                    // Check for loading / saving the scene
+                    if (Context.Input.GetKeyPress(Key.KeyF5))
+                    {
+                        var saveFile = new File(Context, Context.FileSystem.ProgramDir + "Data/Scenes/CharacterDemo.xml", FileMode.FileWrite);
+                        Scene.SaveXML(saveFile);
+                    }
+                    if (Context.Input.GetKeyPress(Key.KeyF7))
+                    {
+                        var loadFile = new File(Context, Context.FileSystem.ProgramDir + "Data/Scenes/CharacterDemo.xml", FileMode.FileRead);
+                        Scene.LoadXML(loadFile);
+                        // After loading we have to reacquire the weak pointer to the Character component, as it has been recreated
+                        // Simply find the character's scene node by name as there's only one of them
+                        var characterNode = Scene.GetChild("Jack", true);
+                        if (characterNode != null)
+                            character_ = characterNode.GetComponent<Character>();
+                    }
+                }
+            }
+        }
+
+        protected void HandlePostUpdate(StringHash eventType, VariantMap eventData)
+        {
+            if (character_ == null)
+                return;
+
+            var characterNode = character_.Node;
+
+            // Get camera lookat dir from character yaw + pitch
+            var rot = characterNode.Rotation;
+            Quaternion dir = rot * new Quaternion(character_.Controls.Pitch, Vector3.Right);
+
+            // Turn head to camera pitch, but limit to avoid unnatural animation
+            var headNode = characterNode.GetChild("Mutant:Head", true);
+            float limitPitch = MathDefs.Clamp(character_.Controls.Pitch, -45.0f, 45.0f);
+            Quaternion headDir = rot * new Quaternion(limitPitch, new Vector3(1.0f, 0.0f, 0.0f));
+            // This could be expanded to look at an arbitrary target, now just look at a point in front
+            // TODO
+            // Vector3 headWorldTarget = headNode.WorldPosition + headDir * new Vector3(0.0f, 0.0f, -1.0f);
+            // headNode.LookAt(headWorldTarget, new Vector3(0.0f, 1.0f, 0.0f));
+
+            if (firstPerson_)
+            {
+                CameraNode.Position = headNode.WorldPosition + rot * new Vector3(0.0f, 0.15f, 0.2f);
+                CameraNode.Rotation = dir;
+            }
+            else
+            {
+                // Third person camera: position behind the character
+                Vector3 aimPoint = characterNode.Position + rot * new Vector3(0.0f, 1.7f, 0.0f);
+
+                // Collide camera ray with static physics objects (layer bitmask 2) to ensure we see the character properly
+                Vector3 rayDir = dir * Vector3.Back;
+                float rayDistance = touch_ != null ? touch_.CameraDistance : Touch.CAMERA_INITIAL_DIST;
+                var result = new PhysicsRaycastResult();
+                Scene.GetComponent<PhysicsWorld>().RaycastSingle(result, new Ray(aimPoint, rayDir), rayDistance, 2);
+                if (result.Body != null)
+                    rayDistance = Math.Min(rayDistance, result.Distance);
+                rayDistance = MathDefs.Clamp(rayDistance, Touch.CAMERA_MIN_DIST, Touch.CAMERA_MAX_DIST);
+
+                CameraNode.Position = aimPoint + rayDir * rayDistance;
+                CameraNode.Rotation = dir;
+            }
+        }
+    }
+}
---
+++ src/Urho3DNet.Samples/Components/Character.cs
@@ -0,0 +1,153 @@
+namespace Urho3DNet.Samples
+{
+    class Character : LogicComponent
+    {
+        public const uint CTRL_FORWARD = 1;
+        public const uint CTRL_BACK = 2;
+        public const uint CTRL_LEFT = 4;
+        public const uint CTRL_RIGHT = 8;
+        public const uint CTRL_JUMP = 16;
+
+        public const float MOVE_FORCE = 0.8f;
+        public const float INAIR_MOVE_FORCE = 0.02f;
+        public const float BRAKE_FORCE = 0.2f;
+        public const float JUMP_FORCE = 7.0f;
+        public const float YAW_SENSITIVITY = 0.1f;
+        public const float INAIR_THRESHOLD_TIME = 0.1f;
+
+        /// Movement controls. Assigned by the main program each frame.
+        public Controls Controls { get; set; } = new Controls();
+        [SerializeField(Name = "Controls Yaw")]
+        private float InternalControlsYaw { get { return Controls.Yaw; } set { Controls.Yaw = value; } }
+        [SerializeField(Name = "Controls Pitch")]
+        private float InternalControlsPitch { get { return Controls.Pitch; } set { Controls.Pitch = value; } }
+
+        /// Grounded flag for movement.
+        [SerializeField(Name = "On Ground")]
+        private bool onGround;
+        /// Jump flag.
+        [SerializeField(Name = "OK To Jump")]
+        private bool okToJump = true;
+        /// In air timer. Due to possible physics inaccuracy, character can be off ground for max. 1/10 second and still be allowed to move.
+        [SerializeField(Name = "In Air Timer")]
+        private float inAirTimer;
+
+        public Character(Context context) : base(context)
+        {
+            // Only the physics update event is needed: unsubscribe from the rest for optimization
+            UpdateEventMask = UpdateEvent.UseFixedupdate;
+        }
+
+        public static new void RegisterObject(Context context)
+        {
+            context.RegisterFactory<Character>();
+        }
+
+        public override void Start()
+        {
+            // Component has been inserted into its scene node. Subscribe to events now
+            SubscribeToEvent(E.NodeCollision, Node, HandleNodeCollision);
+        }
+
+        public override void FixedUpdate(float timeStep)
+        {
+            /// \todo Could cache the components for faster access instead of finding them each frame
+            var body = Node.GetComponent<RigidBody>();
+            var animCtrl = Node.GetComponent<AnimationController>(true);
+
+            // Update the in air timer. Reset if grounded
+            if (!onGround)
+                inAirTimer += timeStep;
+            else
+                inAirTimer = 0.0f;
+            // When character has been in air less than 1/10 second, it's still interpreted as being on ground
+            bool softGrounded = inAirTimer < INAIR_THRESHOLD_TIME;
+
+            // Update movement & animation
+            var rot = Node.Rotation;
+            Vector3 moveDir = Vector3.Zero;
+            var velocity = body.LinearVelocity;
+            // Velocity on the XZ plane
+            var planeVelocity = new Vector3(velocity.X, 0.0f, velocity.Z);
+
+            if (Controls.IsDown(CTRL_FORWARD))
+                moveDir += Vector3.Forward;
+            if (Controls.IsDown(CTRL_BACK))
+                moveDir += Vector3.Back;
+            if (Controls.IsDown(CTRL_LEFT))
+                moveDir += Vector3.Left;
+            if (Controls.IsDown(CTRL_RIGHT))
+                moveDir += Vector3.Right;
+
+            // Normalize move vector so that diagonal strafing is not faster
+            if (moveDir.LengthSquared > 0.0f)
+                moveDir.Normalize();
+
+            // If in air, allow control, but slower than when on ground
+            body.ApplyImpulse(rot * moveDir * (softGrounded ? MOVE_FORCE : INAIR_MOVE_FORCE));
+
+            if (softGrounded)
+            {
+                // When on ground, apply a braking force to limit maximum ground velocity
+                Vector3 brakeForce = -planeVelocity * BRAKE_FORCE;
+                body.ApplyImpulse(brakeForce); // TODO: something is going wrong here
+
+                // Jump. Must release jump control between jumps
+                if (Controls.IsDown(CTRL_JUMP))
+                {
+                    if (okToJump)
+                    {
+                        body.ApplyImpulse(Vector3.Up * JUMP_FORCE);
+                        okToJump = false;
+                        animCtrl.PlayExclusive("Models/Mutant/Mutant_Jump1.ani", 0, false, 0.2f);
+                    }
+                }
+                else
+                    okToJump = true;
+            }
+
+            if (!onGround)
+            {
+                animCtrl.PlayExclusive("Models/Mutant/Mutant_Jump1.ani", 0, false, 0.2f);
+            }
+            else
+            {
+                // Play walk animation if moving on ground, otherwise fade it out
+                if (softGrounded && !moveDir.Equals(Vector3.Zero))
+                    animCtrl.PlayExclusive("Models/Mutant/Mutant_Run.ani", 0, true, 0.2f);
+                else
+                    animCtrl.PlayExclusive("Models/Mutant/Mutant_Idle0.ani", 0, true, 0.2f);
+
+                // Set walk animation speed proportional to velocity
+                animCtrl.SetSpeed("Models/Mutant/Mutant_Run.ani", planeVelocity.Length * 0.3f);
+            }
+
+            // Reset grounded flag for next frame
+            onGround = false;
+        }
+
+        private void HandleNodeCollision(StringHash eventType, VariantMap eventData)
+        {
+            // Check collision contacts and see if character is standing on ground (look for a contact that has near vertical normal)
+            var contacts = new MemoryBuffer(eventData[E.NodeCollision.Contacts].Buffer);
+
+            while (!contacts.IsEof())
+            {
+                Vector3 contactPosition = contacts.ReadVector3();
+                Vector3 contactNormal = contacts.ReadVector3();
+                /*float contactDistance = */
+                contacts.ReadFloat();
+                /*float contactImpulse = */
+                contacts.ReadFloat();
+
+                // If contact is below node center and pointing up, assume it's a ground contact
+                if (contactPosition.Y < (Node.Position.Y + 1.0f))
+                {
+                    float level = contactNormal.Y;
+                    if (level > 0.75)
+                        onGround = true;
+                }
+            }
+        }
+    }
+}
